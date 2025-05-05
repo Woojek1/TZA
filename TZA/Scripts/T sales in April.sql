14731221295892da73f4b0ec368d4f2accfed0f3
Woojek1 <wojciklukaszk@gmail.com>
2025-05-05 15:22:03 +0200

    T sales in April

--- TZA/Scripts/T sales in April.sql
+++ TZA/Scripts/T sales in April.sql
@@ -1,10 +1,10 @@
-WITH symbole_N...A AS (
+WITH symbole_NOXA AS (
 	SELECT 
 		[No_]
 		,[Manufacturer Code]
-	FROM [BC_DEV_cycle].dbo.[N...n$Item$437dbf0e-84ff-417a-965d-ed2bb9650972]
+	FROM [BC_DEV_cycle].dbo.[Nabilaton$Item$437dbf0e-84ff-417a-965d-ed2bb9650972]
 	WHERE 
-		[Manufacturer Code] = 'N...A'
+		[Manufacturer Code] = 'NOXA'
 ),
 
 faktury_kwiecien AS (
@@ -16,17 +16,26 @@ faktury_kwiecien AS (
 		,[Location Code]
 		,[Posting Date]
 		,[Type]
+		,[Sell-to Customer No_]
 	FROM
-		BC_DEV_cycle.dbo.[N...n$Sales Invoice Line$437dbf0e-84ff-417a-965d-ed2bb9650972]
+		BC_DEV_cycle.dbo.[Nabilaton$Sales Invoice Line$437dbf0e-84ff-417a-965d-ed2bb9650972]
 	WHERE
 		[Posting Date] BETWEEN '2025-04-01' AND '2025-04-30'
 		AND [Location Code] = 'MK.GL'
-)
+),
 
+klient AS (
+	SELECT
+		[No_]
+		,[Name]
+	FROM
+		BC_DEV_cycle.dbo.[Nabilaton$Customer$437dbf0e-84ff-417a-965d-ed2bb9650972]
+)
 
 SELECT
 	fk.[No_] AS SYMBOL
 	,fk.[Document No_] AS [NR FAKTURY]
+	,MIN(k.[Name]) AS [KLIENT]
 	,SUM(fk.[Quantity]) AS ILOSC
 	,SUM(fk.[Amount]) AS WARTOSC
 FROM
@@ -34,6 +43,9 @@ FROM
 INNER JOIN
 	symbole_NOXA sn
 ON fk.[No_] = sn.[No_]
+INNER JOIN
+	klient AS k
+ON fk.[Sell-to Customer No_] = k.[No_]
 GROUP BY
 	fk.[No_]
 	,fk.[Document No_]
