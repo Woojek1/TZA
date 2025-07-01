14d1f83689f4791a485cecb472e8614870f90ead
Łukasz <wojciklukaszk@gmail.com>
2025-07-01 17:14:26 +0200

    Sales in export department in Q2

--- TZA/Scripts/Sales in export.sql
+++ TZA/Scripts/Sales in export.sql
@@ -2,6 +2,7 @@ select *
 from
 	gold.v_bc_posted_sales_invoices vbpsi
 where
-	"Salesperson" in ('JGLOWACKA', 'JDUTKIEWIC', 'ASZYDLOWSKI', 'MDRAG')
-AND EXTRACT(MONTH FROM "PostingDate") in (4,5)
+--	"Salesperson" in ('JGLOWACKA', 'JDUTKIEWIC', 'ASZYDLOWSKI', 'MDRAG')
+--AND 
+EXTRACT(MONTH FROM "PostingDate") in (6)
 AND EXTRACT(YEAR FROM "PostingDate") = 2025
