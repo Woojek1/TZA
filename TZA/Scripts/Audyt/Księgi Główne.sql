540a85292129c5d5a73872c166ecc028238ffdca
Łukasz <wojciklukaszk@gmail.com>
2025-08-13 14:06:38 +0200

    Add General Ledger Report

---
+++ TZA/Scripts/Audyt/Księgi Główne.sql
@@ -0,0 +1,18 @@
+SELECT
+	"Entry No_"
+	,"G_L Account No_"
+	,cast("Posting Date" as date) as "Posting Date"
+	,"Document No_"
+	,"Description"
+	,"External Document No_"
+	,"Debit Amount"
+	,"Credit Amount"
+FROM
+	BC_DEV_cycle.dbo.[Zymetric$G_L Entry$437dbf0e-84ff-417a-965d-ed2bb9650972]
+WHERE
+	[Posting Date] >= '2024-04-01'
+AND
+	[Posting Date] <= '2025-03-31'
+order by
+	[Entry No_] asc
+--	"Posting Date" asc
