a4505cc70c346738b6e406c5f9dd0f7990ed7f29
Łukasz <wojciklukaszk@gmail.com>
2025-08-29 11:01:44 +0200

    Indirect costs report

---
+++ TZA/Scripts/Audyt/Koszty pośrednie.sql
@@ -0,0 +1,17 @@
+SELECT
+	"Posting_Date" as "Data księgowania"
+	,"Item_Ledger_Entry_Type" as "Typ zapisu księgi zapasów"
+	,"Entry_Type" as "Typ zapisu"
+	,"Document_No" as "Nr dokumentu"
+	,"Item_Charge_No" as "Nr kosztu dodatkowego"
+	,"Description" as "Opis"
+	,"Cost_Amount_Expected" as "Kwota kosztu (przewidywana)"
+	,"Cost_Amount_Actual" as "Kwota kosztu (rzeczywista)"
+	,"Cost_Posted_to_G_L" as "Koszt zaksięgowany w K/G"
+	,"Gen_Prod_Posting_Group" as "Gł. tow. grupa księgowa"
+FROM
+	bronze.bc_values_entries_zymetric
+where
+	"Item_Ledger_Entry_Type" = 'Purchase'
+and
+	"Posting_Date" BETWEEN '01-04-2024' and '31-05-2024'
